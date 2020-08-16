1ed19a3357252d5c233ed3b04e3606fe9425c8e4
CodeArc13 <codearc13@gmail.com>
2020-08-16 12:09:03 +0100

    remove combo keys code, add multiple keys code and new def

--- MyKSPTools.py
+++ MyKSPTools.py
@@ -3,16 +3,16 @@ from pynput.keyboard import Key, Controller
 import clipboard
 import time
 
-COMBINATIONS = [
-    {keyboard.KeyCode(char='`')}
-]
+#COMBINATIONS = [
+#    {keyboard.KeyCode(char='/')}
+#]
 
 # The currently active modifiers
-current = set()
+#current = set()
 
 kb = Controller()
 
-def execute():
+def timeAndDate():
     clipboard.copy(time.strftime('%d-%m-%Y %H-%M-%S', time.localtime()) + ' ')
     kb.press(Key.backspace)
     kb.release(Key.backspace) 
@@ -21,15 +21,25 @@ def execute():
     kb.release('v')
     kb.release(Key.ctrl.value)
 
+def rootStrutShipFile():
+    pass
+
 def on_press(key):
-    if any([key in COMBO for COMBO in COMBINATIONS]):
-        current.add(key)
-        if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS):
-            execute()
+    print(key)
+    if key == keyboard.Key.insert:
+        timeAndDate()
+    if key == keyboard.Key.delete:
+        rootStrutShipFile()
+
+    #if any([key in COMBO for COMBO in COMBINATIONS]):
+    #    current.add(key)
+    #    if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS):
+    #        execute()
 
 def on_release(key):
-    if any([key in COMBO for COMBO in COMBINATIONS]):
-        current.remove(key)
+    pass
+    #if any([key in COMBO for COMBO in COMBINATIONS]):
+    #    current.remove(key)
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
